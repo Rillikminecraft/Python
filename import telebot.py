22f4937c6d5c1e40363aeb89705074c59006fcc6
Rillikminecraft <151042075+Rillikminecraft@users.noreply.github.com>
2023-12-22 21:07:12 +0300

    Start/Stop SRC TG.py

--- import telebot.py
+++ import telebot.py
@@ -1,17 +1,17 @@
-import telebot
-from telebot import types
+import telebot # импортирует библиотеку тг бота
+from telebot import types # импортирует types из telebot (для кнопок)
 
-bot = telebot.TeleBot('')
+bot = telebot.TeleBot('') # сюда вставляем токен
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, 'Вы запустили бота!')
+@bot.message_handler(commands=['start']) # команда /start
+def start(message): # функция старта
+    bot.send_message(message.chat.id, 'Вы запустили бота!') # сообщение от бота
 
-@bot.message_handler(commands=['stop'])
-def stop(message):
-    keyboard_markup = types.ReplyKeyboardMarkup(row_width=1)
-    btn1 = types.KeyboardButton('/start')
-    keyboard_markup.add(btn1)
-    bot.send_message(message.chat.id, 'Вы остановили бота, надеемся вы вернетесь.', reply_markup=keyboard_markup)
+@bot.message_handler(commands=['stop']) # команда /stop
+def stop(message): # функция стопа
+    keyboard_markup = types.ReplyKeyboardMarkup(row_width=1) # обработка кнопок
+    btn1 = types.KeyboardButton('/start') # создание кнопки
+    keyboard_markup.add(btn1) # подключение кнопки
+    bot.send_message(message.chat.id, 'Вы остановили бота, надеемся вы вернетесь.', reply_markup=keyboard_markup) # сообщение + вывод кнопки
 
-bot.polling(none_stop=True)
+bot.polling(none_stop=True) # запускает цикл
